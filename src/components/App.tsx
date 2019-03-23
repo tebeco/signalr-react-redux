4514b866c482b40f125da10c61ccebd1ff88885e
tebeco <tebeco@gmail.com>
2019-03-23 19:02:16 +0100

    [react/redux] Display connectivity and generate proper DOM based on it

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { stat } from 'fs';
+import { RootState, StreamingConnectivityState } from '../store/rootState';
 
-export const App = () => {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
+type AppProps = {
+  appConnectivity: StreamingConnectivityState;
+};
+
+const mapStateToProps = (state: RootState): AppProps => ({
+  appConnectivity: state.streaming.connectivity
+});
+
+const connectButton = <button>Connect</button>
+const disconnectButton = <button>Disconnect</button>
+const noConnectivityButton = <></>
+
+const getConnectivityButton = (appConnectivity: StreamingConnectivityState): JSX.Element => {
+  switch (appConnectivity) {
+    case 'Connected':
+    case 'Connecting':
+      return disconnectButton;
+    case 'Disconnected':
+      return connectButton;
+    case 'Disconnecting':
+      return noConnectivityButton;
+    default:
+      throw "unhandled connectivity state";
   }
+}
+
+const AppComponent = (props: AppProps) => {
+  const changeConnectivitybutton = getConnectivityButton(props.appConnectivity);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Current connectivity : <code>{props.appConnectivity}</code>.
+        </p>
+        {changeConnectivitybutton}
+      </header>
+    </div>
+  );
+}
+
+export const App = connect(mapStateToProps)(AppComponent)
