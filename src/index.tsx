87da26187fdb831f0125e178a926c6b31d038f3c
tebeco <tebeco@gmail.com>
2019-03-23 13:56:55 +0100

    [redux] Create the store at bootstrap and pass it to the React App

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { configureStore } from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create the store
+const store = configureStore()
+
+// Get the root DOM element
+const rootElement = document.getElementById('root');
+
+// Render the app
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
