6d93ff47e3a5d6015552bcec6967fa35c2b5fa82
tebeco <tebeco@gmail.com>
2019-03-31 13:13:52 +0200

    [typescript] use string instead of type for Actions

--- front-app/src/reducers/streamingActions.ts
+++ front-app/src/reducers/streamingActions.ts
@@ -1,19 +1,25 @@
-export type StreamingActions = 
+export type StreamingActions =
     ConnectAction |
     ConnectedAction |
-    DisconnectAction;
+    DisconnectAction |
+    DisconnectedAction;
 
-export type STREAMING_CONNECT_ACTION = 'STREAMING_CONNECT_ACTION';
+export const STREAMING_CONNECT_ACTION = 'STREAMING_CONNECT_ACTION';
 export type ConnectAction = {
-    type: STREAMING_CONNECT_ACTION
+    type: 'STREAMING_CONNECT_ACTION'
 };
 
-export type STREAMING_CONNECTED_ACTION = 'STREAMING_CONNECTED_ACTION'; 
+export const STREAMING_CONNECTED_ACTION = 'STREAMING_CONNECTED_ACTION';
 export type ConnectedAction = {
-    type: STREAMING_CONNECTED_ACTION
+    type: 'STREAMING_CONNECTED_ACTION'
 };
 
-export type STREAMING_DISCONNECT_ACTION = 'STREAMING_DISCONNECT_ACTION'; 
+export const STREAMING_DISCONNECT_ACTION = 'STREAMING_DISCONNECT_ACTION';
 export type DisconnectAction = {
-    type: STREAMING_DISCONNECT_ACTION
+    type: 'STREAMING_DISCONNECT_ACTION'
+};
+
+export const STREAMING_DISCONNECTED_ACTION = 'STREAMING_DISCONNECTED_ACTION';
+export type DisconnectedAction = {
+    type: 'STREAMING_DISCONNECTED_ACTION'
 };
--- front-app/src/reducers/streamingReducer.ts
+++ front-app/src/reducers/streamingReducer.ts
@@ -1,22 +1,22 @@
-import { StreamingState, initialStreamingState } from "../store/rootState";
+import { StreamingState, initialStreamingState } from '../store/rootState';
 import { StreamingActions } from "./streamingActions";
 
 export const streamingReducer = (state: StreamingState = initialStreamingState, action: StreamingActions): StreamingState => {
     switch (action.type) {
-        case "STREAMING_CONNECT_ACTION":
+        case 'STREAMING_CONNECT_ACTION':
             return {
                 ...state,
-                connectivity: "Connecting"
+                connectivity: 'Connecting'
             };
-        case "STREAMING_CONNECTED_ACTION":
+        case 'STREAMING_CONNECTED_ACTION':
             return {
                 ...state,
-                connectivity: "Connected"
+                connectivity: 'Connected'
             };
-        case "STREAMING_DISCONNECT_ACTION":
+        case 'STREAMING_DISCONNECT_ACTION':
             return {
                 ...state,
-                connectivity: "Disconnected"
+                connectivity: 'Disconnected'
             };
         default:
             return state
