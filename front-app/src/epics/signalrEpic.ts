eab4f43a6482865fa584b4341c1fdec8cb63b65c
tebeco <tebeco@gmail.com>
2019-04-07 22:07:15 +0200

    [redux-observable] Implement signalr connection lifecycle / remove debug button

--- front-app/src/epics/signalrEpic.ts
+++ front-app/src/epics/signalrEpic.ts
@@ -1,46 +1,77 @@
-import { Observable, Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, defer, Subject } from 'rxjs';
+import { mergeMap, map } from 'rxjs/operators';
 import { RootState } from '../store/rootState';
 import { RootActions } from '../store/rootActions';
-import { HubConnectionBuilder, LogLevel } from '@aspnet/signalr'
+import { HubConnectionBuilder, LogLevel, HubConnection } from '@aspnet/signalr'
 import { ofType } from 'redux-observable';
-import { ConnectAction } from '../reducers/streamingActions';
+import { STREAMING_CONNECT_ACTION, STREAMING_CONNECTED_ACTION, STREAMING_DISCONNECT_ACTION, STREAMING_DISCONNECTED_ACTION, DisconnectedAction } from '../reducers/streamingActions';
+import { StreamingActions } from '../reducers/streamingActions';
 
 
 export const createSignalrEpic = () => (actions$: Observable<RootActions>, state$: Observable<RootState>) => {
     //Create subject
     const signalrEpicSubject = new Subject<RootActions>();
 
-    //connect ws 
     const hubConnection = new HubConnectionBuilder()
         .configureLogging(LogLevel.Debug)
-        .withUrl("https://localhost:5001/clients")
+        .withUrl("https://localhost:5043/clients")
         .build();
 
-    actions$.pipe(
-        ofType('STREAMING_CONNECT_ACTION'),
-        tap<ConnectAction>(() => {
-            hubConnection.start()
-                .then(() => {
-                    signalrEpicSubject.next({
-                        type: 'STREAMING_CONNECTED_ACTION'
-                    });
-                })
-                .catch((err) => {
-                    console.log(err)
-                    signalrEpicSubject.next({
-                        type: 'STREAMING_DISCONNECTED_ACTION'
-                    });
-                });
-        })
-    );
+    const connect$ = handleConnectAction(hubConnection, actions$, state$);
+    const diconnect$ = handleDisconnectAction(hubConnection, actions$, state$);
 
-    return signalrEpicSubject.asObservable();
+    connect$.subscribe({ next: (action) => signalrEpicSubject.next(action) });
+    diconnect$.subscribe({ next: (action) => signalrEpicSubject.next(action) });
+
+    hubConnection.onclose((err) => signalrEpicSubject.next({
+        type: STREAMING_DISCONNECTED_ACTION
+    }));
+
+    return signalrEpicSubject;
+};
+
+const handleConnectAction = (hubConnection: HubConnection, actions$: Observable<RootActions>, state$: Observable<RootState>): Observable<StreamingActions> => {
+    return actions$.pipe(
+        ofType(STREAMING_CONNECT_ACTION),
+        mergeMap(_ => {
+
+            return defer(async () => {
+                try {
+                    await hubConnection.start()
+                    return ({
+                        type: STREAMING_CONNECTED_ACTION
+                    });
+                } catch (error) {
+                    return ({
+                        type: STREAMING_DISCONNECTED_ACTION
+                    });
+                }
+            });
+        }),
+        map(action => {
+            return action as StreamingActions
+        })
+    )
 };
 
 
+const handleDisconnectAction = (hubConnection: HubConnection, actions$: Observable<RootActions>, state$: Observable<RootState>): Observable<StreamingActions> => {
+    return actions$.pipe(
+        ofType(STREAMING_DISCONNECT_ACTION),
+        mergeMap(_ => {
+            return defer(async () => {
+                await hubConnection.stop();
 
-
+                return ({
+                    type: STREAMING_DISCONNECTED_ACTION
+                });
+            });
+        }),
+        map(action => {
+            return action as DisconnectedAction
+        })
+    )
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                          SUBSCRIBE TO STREAM                                           //
