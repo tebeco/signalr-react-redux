7bd811580521eb2bac6ba7b8dd600eb0463435b2
tebeco <tebeco@gmail.com>
2019-04-21 10:33:15 +0200

    embracing previews for front-webapi

--- front-webapi/src/Startup.cs
+++ front-webapi/src/Startup.cs
@@ -32,7 +32,8 @@ namespace Front.WebApi
                 .AddNewtonsoftJson();
 
             services.AddCors();
-            services.AddSignalR();
+            services.AddSignalR()
+                ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
