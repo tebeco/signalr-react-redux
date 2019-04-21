e16a0f7743695c6e779dda90536b662e2b19f1b7
tebeco <tebeco@gmail.com>
2019-04-21 10:33:25 +0200

    adding streaming api

---
+++ streaming-webapi/src/FrontClientHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Streaming.WebApi
+{
+    public class FrontClientHub : Hub
+    {
+        private Dictionary<string, InfiniteProductPublisher> infiniteProductPublisher = new Dictionary<string, InfiniteProductPublisher>();
+
+
+        public ChannelReader<Product> SubscribeToInfiniteProduct(string productId)
+        {
+            if (!infiniteProductPublisher.ContainsKey(productId))
+            {
+                lock (infiniteProductPublisher)
+                {
+                    if (!infiniteProductPublisher.ContainsKey(productId))
+                    {
+                        infiniteProductPublisher[productId] = new InfiniteProductPublisher(productId, TimeSpan.FromMilliseconds(200));
+                    }
+                }
+            }
+            var publisher = infiniteProductPublisher[productId];
+
+            return publisher.ProductChannel.Reader;
+        }
+
+        public ChannelReader<Product> SubscribeToLimitedProduct(string productId)
+        {
+            var publisher = new LimitedProductPublisher(productId, 3, TimeSpan.FromMilliseconds(200));
+            return publisher.ProductChannel.Reader;
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            var publishers = infiniteProductPublisher.Values.ToList();
+            infiniteProductPublisher = new Dictionary<string, InfiniteProductPublisher>();
+
+            publishers.ForEach(publisher => publisher.Dispose());
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ streaming-webapi/src/InfiniteProductPublisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace Streaming.WebApi
+{
+    public class InfiniteProductPublisher : IDisposable
+    {
+        private readonly IDisposable _subscription;
+
+        public Channel<Product> ProductChannel { get; }
+
+        public InfiniteProductPublisher(string productId, TimeSpan interval)
+        {
+            ProductChannel = Channel.CreateUnbounded<Product>();
+            _subscription = Observable.Interval(interval)
+                                    .Select((_, index) => Product.GetRandomProduct(productId))
+                                    .Subscribe(
+                                       product => ProductChannel.Writer.TryWrite(product),
+                                       _ => ProductChannel.Writer.Complete(),
+                                       () => ProductChannel.Writer.Complete()
+                                    );
+        }
+
+
+        public void Dispose()
+        {
+            ProductChannel.Writer.Complete();
+            _subscription.Dispose();
+        }
+    }
+}
