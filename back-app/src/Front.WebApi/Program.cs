03f020bc64b39798876d74d51fabfbf77493522d
TeBeCo <tebeco@gmail.com>
2020-05-13 10:36:00 +0200

    Add code du Debug SignalR Client from CSharp with System.Text.Json / MessagePack / Newtonsoft.Json

--- back-app/src/Front.WebApi/Program.cs
+++ back-app/src/Front.WebApi/Program.cs
@@ -4,9 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Front.WebApi.Hubs;
+using Front.WebApi.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +21,10 @@ namespace Front.WebApi
     {
         public static void Main(string[] args)
         {
+#if DEBUG_FROM_CSHARP_Client
+            DebugCSharpClient();
+#endif
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -25,5 +34,33 @@ namespace Front.WebApi
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static void DebugCSharpClient()
+        {
+            StartClient<ISignalRBuilder>((hubConnectionBuilder) => { hubConnectionBuilder.AddMessagePackProtocol(); return "   MSGPACK"; });
+            StartClient<ISignalRBuilder>((hubConnectionBuilder) => { hubConnectionBuilder.AddJsonProtocol(); return "      JSON"; });
+            StartClient<ISignalRBuilder>((hubConnectionBuilder) => { hubConnectionBuilder.AddNewtonsoftJsonProtocol(); return "NEWTONSOFT"; });
+        }
+
+        public static void StartClient<T>(Func<ISignalRBuilder, string> addProtocol) where T : ISignalRBuilder
+        {
+            var hubConnectionBuilderBuilder = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5043/clients")
+                .WithAutomaticReconnect()
+                ;
+            var protocolName = addProtocol(hubConnectionBuilderBuilder);
+            var hubConnection = hubConnectionBuilderBuilder.Build();
+
+            Task.Run(async () =>
+            {
+                await hubConnection.StartAsync();
+                var channel = await hubConnection.StreamAsChannelAsync<Product>(nameof(FrontClientHub.SubscribeToInfiniteProduct), "EUR/USD");
+                while (true)
+                {
+                    var product = await channel.ReadAsync();
+                    Console.WriteLine($"{protocolName} ---- {product.ProductId} : '{product.Price}'");
+                }
+            });
+        }
     }
 }
